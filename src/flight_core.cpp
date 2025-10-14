9a4bd7de3a8c260886ad6a2674ece2daaf86a916
Benjamin Low <benlow999@gmail.com>
2025-10-14 05:28:40 +0000

    feat: revert to single threaded executor

--- src/flight_core.cpp
+++ src/flight_core.cpp
@@ -16,8 +16,6 @@
 #include <iostream>
 #include <thread>
 #include <cmath>
-#include <mutex>
-#include <vector>
 
 enum class CoreMode
 {
@@ -26,12 +24,6 @@ enum class CoreMode
     TRAJ_PATH,
     OVERRIDE,
     OUT_OF_BOUNDS
-};
-
-struct VehicleCommandResult
-{
-    bool done;
-    uint8_t result;
 };
 
 class FlightCore : public rclcpp::Node
@@ -46,7 +38,6 @@ public:
 
         rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
         auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
-        service_call_group_ = create_callback_group(rclcpp::CallbackGroupType::Reentrant);
 
         // PX4
         vehicle_status_subscription_ = this->create_subscription<px4_msgs::msg::VehicleStatus>("/fmu/out/vehicle_status_v1", qos, std::bind(&FlightCore::vehicle_status_subscription_callback, this, std::placeholders::_1));
@@ -57,7 +48,7 @@ public:
         trajectory_setpoint_publisher_ = this->create_publisher<px4_msgs::msg::TrajectorySetpoint>("/fmu/in/trajectory_setpoint", 10);
 
         // TODO: prune stale service requests
-        vehicle_command_client_ = this->create_client<px4_msgs::srv::VehicleCommand>("/fmu/vehicle_command", rmw_qos_profile_services_default, service_call_group_);
+        vehicle_command_client_ = this->create_client<px4_msgs::srv::VehicleCommand>("/fmu/vehicle_command", rmw_qos_profile_services_default);
 
         // FlightCore
         goto_setpoint_subscriber_ = this->create_subscription<px4_msgs::msg::GotoSetpoint>("/uas/core/goto_setpoint", qos, std::bind(&FlightCore::goto_setpoint_callback, this, std::placeholders::_1));
@@ -65,7 +56,7 @@ public:
 
         core_status_publisher_ = this->create_publisher<flight_stack_msgs::msg::CoreStatus>("/uas/core/status", 10);
 
-        core_command_service_ = this->create_service<flight_stack_msgs::srv::CoreCommand>("/uas/core/command", std::bind(&FlightCore::core_command_callback, this, std::placeholders::_1, std::placeholders::_2));
+        core_command_service_ = this->create_service<flight_stack_msgs::srv::CoreCommand>("/uas/core/command", std::bind(&FlightCore::core_command_callback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
 
         // Startup
         RCLCPP_INFO_STREAM(this->get_logger(), "Setting up FlightCore");
@@ -118,10 +109,14 @@ private:
     void publish_trajectory_setpoint_raw(px4_msgs::msg::TrajectorySetpoint msg);
 
     rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedPtr vehicle_command_client_;
-    int vehicle_command_request(uint16_t command, float param1, float param2);
-    void vehicle_command_callback(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future, int id);
-    std::vector<VehicleCommandResult> vehicle_command_results_;
-    std::mutex vehicle_command_results_mutex_;
+    void vehicle_command_request(uint16_t command, float param1, float param2);
+    void vehicle_command_callback(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future);
+    void vehicle_command_request(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                                 const std::shared_ptr<rmw_request_id_t> request_header,
+                                 uint16_t command, float param1, float param2);
+    void vehicle_command_callback(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                                  const std::shared_ptr<rmw_request_id_t> request_header,
+                                  rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future);
 
     // FlightCore pub/subs ...
     rclcpp::Subscription<px4_msgs::msg::GotoSetpoint>::SharedPtr goto_setpoint_subscriber_;
@@ -135,13 +130,24 @@ private:
     void publish_status();
 
     rclcpp::Service<flight_stack_msgs::srv::CoreCommand>::SharedPtr core_command_service_;
-    void core_command_callback(const std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Request> request, std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Response> response);
+    void core_command_callback(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                               const std::shared_ptr<rmw_request_id_t> request_header,
+                               const std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Request> request_msg);
 
     // FlightCore functions
-    int arm();
-    int disarm();
-    int land();
-    int mode(float mode);
+    void arm();
+    void disarm();
+    void land();
+    void mode(float mode);
+    void arm(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+             const std::shared_ptr<rmw_request_id_t> request_header);
+    void disarm(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                const std::shared_ptr<rmw_request_id_t> request_header);
+    void land(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+              const std::shared_ptr<rmw_request_id_t> request_header);
+    void mode(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+              const std::shared_ptr<rmw_request_id_t> request_header,
+              float mode);
 
     void control_timer_callback();
 };
@@ -221,7 +227,7 @@ void FlightCore::publish_trajectory_setpoint_raw(px4_msgs::msg::TrajectorySetpoi
     trajectory_setpoint_publisher_->publish(msg);
 }
 
-int FlightCore::vehicle_command_request(uint16_t command, float param1 = 0.0, float param2 = 0.0)
+void FlightCore::vehicle_command_request(uint16_t command, float param1 = 0.0, float param2 = 0.0)
 {
     auto request = std::make_shared<px4_msgs::srv::VehicleCommand::Request>();
 
@@ -237,26 +243,15 @@ int FlightCore::vehicle_command_request(uint16_t command, float param1 = 0.0, fl
     msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
     request->request = msg;
 
-    const std::lock_guard<std::mutex> lock(vehicle_command_results_mutex_);
-    vehicle_command_results_.push_back({false, 0});
-    int id = vehicle_command_results_.size() - 1;
-    auto callback_wrapper = [id, this](rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future)
-    {
-        this->vehicle_command_callback(future, id);
-    };
-    auto result = vehicle_command_client_->async_send_request(request, std::move(callback_wrapper));
-    return id;
+    auto result = vehicle_command_client_->async_send_request(request, std::bind(static_cast<void (FlightCore::*)(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture)>(&FlightCore::vehicle_command_callback), this, std::placeholders::_1));
 }
 
-void FlightCore::vehicle_command_callback(
-    rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future, int id)
+void FlightCore::vehicle_command_callback(rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future)
 {
     auto status = future.wait_for(std::chrono::seconds(1));
     if (status == std::future_status::ready)
     {
-        const std::lock_guard<std::mutex> lock(vehicle_command_results_mutex_);
         auto reply = future.get()->reply;
-        vehicle_command_results_[id].result = reply.result; // is this actually thread safe?
         switch (reply.result)
         {
         case reply.VEHICLE_CMD_RESULT_ACCEPTED:
@@ -284,7 +279,6 @@ void FlightCore::vehicle_command_callback(
             RCLCPP_WARN(this->get_logger(), "command reply unknown");
             break;
         }
-        vehicle_command_results_[id].done = true;
     }
     else
     {
@@ -292,46 +286,112 @@ void FlightCore::vehicle_command_callback(
     }
 }
 
-void FlightCore::core_command_callback(const std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Request> request_msg,
-                                       std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Response> response)
+void FlightCore::vehicle_command_request(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                                         const std::shared_ptr<rmw_request_id_t> request_header,
+                                         uint16_t command, float param1 = 0.0, float param2 = 0.0)
+{
+    auto request = std::make_shared<px4_msgs::srv::VehicleCommand::Request>();
+
+    px4_msgs::msg::VehicleCommand msg{};
+    msg.param1 = param1;
+    msg.param2 = param2;
+    msg.command = command;
+    msg.target_system = 1;
+    msg.target_component = 1;
+    msg.source_system = 1;
+    msg.source_component = 1;
+    msg.from_external = true;
+    msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    request->request = msg;
+
+    auto async_cb = [this, service, request_header](rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future)
+    {
+        this->vehicle_command_callback(service, request_header, future);
+    };
+
+    auto result = vehicle_command_client_->async_send_request(request, async_cb);
+}
+
+void FlightCore::vehicle_command_callback(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                                          const std::shared_ptr<rmw_request_id_t> request_header,
+                                          rclcpp::Client<px4_msgs::srv::VehicleCommand>::SharedFuture future)
+{
+    auto status = future.wait_for(std::chrono::seconds(1));
+    if (status == std::future_status::ready)
+    {
+        auto reply = future.get()->reply;
+        flight_stack_msgs::srv::CoreCommand::Response response;
+        switch (reply.result)
+        {
+        case reply.VEHICLE_CMD_RESULT_ACCEPTED:
+            RCLCPP_INFO(this->get_logger(), "command accepted");
+            response.reply.result = response.reply.RESULT_SUCCESS;
+            service->send_response(*request_header, response);
+            return;
+        case reply.VEHICLE_CMD_RESULT_TEMPORARILY_REJECTED:
+            RCLCPP_WARN(this->get_logger(), "command temporarily rejected");
+            break;
+        case reply.VEHICLE_CMD_RESULT_DENIED:
+            RCLCPP_WARN(this->get_logger(), "command denied");
+            break;
+        case reply.VEHICLE_CMD_RESULT_UNSUPPORTED:
+            RCLCPP_WARN(this->get_logger(), "command unsupported");
+            break;
+        case reply.VEHICLE_CMD_RESULT_FAILED:
+            RCLCPP_WARN(this->get_logger(), "command failed");
+            break;
+        case reply.VEHICLE_CMD_RESULT_IN_PROGRESS:
+            RCLCPP_WARN(this->get_logger(), "command in progress");
+            break;
+        case reply.VEHICLE_CMD_RESULT_CANCELLED:
+            RCLCPP_WARN(this->get_logger(), "command cancelled");
+            break;
+        default:
+            RCLCPP_WARN(this->get_logger(), "command reply unknown");
+            break;
+        }
+        response.reply.result = response.reply.RESULT_FAILURE;
+        service->send_response(*request_header, response);
+    }
+    else
+    {
+        RCLCPP_INFO(this->get_logger(), "Service In-Progress...");
+    }
+}
+
+void FlightCore::core_command_callback(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                                       const std::shared_ptr<rmw_request_id_t> request_header,
+                                       const std::shared_ptr<flight_stack_msgs::srv::CoreCommand::Request> request_msg)
 {
     auto request = request_msg->request;
-    response->reply.command = request.command;
-
-    int request_id;
-    bool service_called{false};
 
     switch (request.command)
     {
     case request.ARM:
         if (is_simulation_ == false)
         {
-            response->reply.result = response->reply.RESULT_FAILURE;
+            flight_stack_msgs::srv::CoreCommand::Response response;
+            response.reply.result = response.reply.RESULT_FAILURE;
+            service->send_response(*request_header, response);
             return;
         }
-        request_id = arm();
-        service_called = true;
-        break;
+        arm(service, request_header);
+        return;
     case request.DISARM:
-        request_id = disarm();
-        service_called = true;
-        break;
+        disarm(service, request_header);
+        return;
     case request.MODE_OFFBOARD:
-        request_id = mode(6.0);
-        service_called = true;
-        break;
+        mode(service, request_header, 6.0);
+        return;
     case request.MODE_POSCTL:
-        request_id = mode(3.0);
-        service_called = true;
-        break;
+        mode(service, request_header, 3.0);
+        return;
     case request.MODE_ALTCTL:
-        request_id = mode(2.0);
-        service_called = true;
-        break;
+        mode(service, request_header, 2.0);
+        return;
     case request.MODE_LAND:
-        request_id = land();
-        service_called = true;
-        break;
+        land(service, request_header);
+        return;
     case request.CORE_GOTO:
         core_mode_ = CoreMode::GOTO;
         break;
@@ -345,57 +405,33 @@ void FlightCore::core_command_callback(const std::shared_ptr<flight_stack_msgs::
         core_mode_ = CoreMode::OVERRIDE;
         break;
     default:
-        response->reply.result = response->reply.RESULT_FAILURE;
+        flight_stack_msgs::srv::CoreCommand::Response response;
+        response.reply.result = response.reply.RESULT_FAILURE;
+        service->send_response(*request_header, response);
         return;
     }
 
-    if (service_called == true)
-    {
-        while (vehicle_command_results_[request_id].done != true)
-        {
-            std::this_thread::yield();
-        }
-        const std::lock_guard<std::mutex> lock(vehicle_command_results_mutex_);
-        if (vehicle_command_results_[request_id].result == px4_msgs::msg::VehicleCommandAck::VEHICLE_CMD_RESULT_ACCEPTED)
-        {
-            response->reply.result = response->reply.RESULT_SUCCESS;
-        }
-        else
-        {
-            response->reply.result = response->reply.RESULT_FAILURE;
-        }
-        vehicle_command_results_.erase(vehicle_command_results_.begin() + request_id);
-        switch (request.command)
-        {
-        case request.MODE_LAND:
-            core_mode_ = CoreMode::OVERRIDE;
-            break;
-        default:
-            break;
-        }
-    }
-    else
-    {
-        response->reply.result = response->reply.RESULT_SUCCESS;
-    }
+    flight_stack_msgs::srv::CoreCommand::Response response;
+    response.reply.result = response.reply.RESULT_SUCCESS;
+    service->send_response(*request_header, response);
 }
 
-int FlightCore::arm()
+void FlightCore::arm()
 {
-    return vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+    vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
 }
 
-int FlightCore::disarm()
+void FlightCore::disarm()
 {
-    return vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
+    vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
 }
 
-int FlightCore::land()
+void FlightCore::land()
 {
-    return vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_LAND, 1.0);
+    vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_LAND, 1.0);
 }
 
-int FlightCore::mode(float mode)
+void FlightCore::mode(float mode)
 {
     // Best explanation I could find here: https://discuss.px4.io/t/switching-modes-in-px4-using-ros2-and-uxrce-dds/37137
     // param1 = 1 for px4 custom modes
@@ -403,7 +439,32 @@ int FlightCore::mode(float mode)
     // 2 -- ALTCTL
     // 3 -- POSCTL
     // 6 -- OFFBOARD
-    return vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1.0, mode);
+    vehicle_command_request(px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1.0, mode);
+}
+
+void FlightCore::arm(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                     const std::shared_ptr<rmw_request_id_t> request_header)
+{
+    vehicle_command_request(service, request_header, px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 1.0);
+}
+
+void FlightCore::disarm(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                        const std::shared_ptr<rmw_request_id_t> request_header)
+{
+    vehicle_command_request(service, request_header, px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM, 0.0);
+}
+
+void FlightCore::land(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                      const std::shared_ptr<rmw_request_id_t> request_header)
+{
+    vehicle_command_request(service, request_header, px4_msgs::msg::VehicleCommand::VEHICLE_CMD_NAV_LAND, 1.0);
+}
+
+void FlightCore::mode(std::shared_ptr<rclcpp::Service<flight_stack_msgs::srv::CoreCommand>> service,
+                      const std::shared_ptr<rmw_request_id_t> request_header,
+                      float mode)
+{
+    vehicle_command_request(service, request_header, px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1.0, mode);
 }
 
 void FlightCore::goto_setpoint_callback(px4_msgs::msg::GotoSetpoint::UniquePtr msg)
@@ -427,6 +488,13 @@ void FlightCore::control_timer_callback()
 {
     publish_offboard_control_mode();
     publish_status();
+    if (is_offboard_ == false)
+    {
+        // THIS IS VERY IMPORTANT NEVER PUBLISH ANY SETPOINTS IF NOT IN OFFBOARD
+        // https://github.com/PX4/PX4-Autopilot/issues/22512
+        // https://github.com/PX4/PX4-Autopilot/pull/22530
+        return;
+    }
     switch (core_mode_)
     {
     case CoreMode::GOTO:
@@ -494,7 +562,7 @@ int main(int argc, char *argv[])
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
     rclcpp::init(argc, argv);
     rclcpp::Node::SharedPtr node1 = std::make_shared<FlightCore>(is_simulation);
-    rclcpp::executors::MultiThreadedExecutor executor;
+    rclcpp::executors::SingleThreadedExecutor executor;
     executor.add_node(node1);
     executor.spin();
 
